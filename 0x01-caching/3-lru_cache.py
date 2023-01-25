7fae23e4acacb67be3e607fe719fe58efbfa4c98
cclutta <chelsea.lutta@gmail.com>
2023-01-25 22:57:10 +0300

    Update

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+LRU Cache Module
+
+Contains LRU Cache class
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRU Cache system. """
+
+    def __init__(self):
+        super().__init__()
+        self.history = []
+
+    def put(self, key, item):
+        """ Put function. """
+        if key and item:
+            self.cache_data[key] = item
+
+            if len(self.cache_data) > self.MAX_ITEMS:
+                delete = self.history[0]
+                print("DISCARD: {}".format(delete))
+                del self.cache_data[delete]
+                self.history.pop(0)
+
+            self.update(key)
+
+    def update(self, key):
+        """ Updates cache history """
+        if key in self.history:
+            self.history.remove(key)
+        self.history.append(key)
+
+    def get(self, key):
+        """ Get function. """
+        if key and key in self.cache_data:
+            return self.cache_data.get(key)
+        return None
