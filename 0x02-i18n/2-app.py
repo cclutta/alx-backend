4d9d8a6002dfc1e85798a247aea8d99888495a9e
cclutta <chelsea.lutta@gmail.com>
2023-01-31 20:28:35 +0300

    Update 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -21,6 +21,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """ Method to get_locale with best match """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
