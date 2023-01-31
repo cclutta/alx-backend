0fff432f02aded521bc29802fe16d41338891e71
cclutta <chelsea.lutta@gmail.com>
2023-01-31 20:07:04 +0300

    Update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """ Simply outputs template"""
-    return render_template('index.html')
+    return render_template('0-index.html')
+
 
 if __name__ == "__main__":
     """ Main Function """
