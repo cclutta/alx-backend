0d5c60f1b19930f2b9f19bb7ab791ad30b8aff32
cclutta <chelsea.lutta@gmail.com>
2023-01-31 20:15:46 +0300

    Update

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Starts a Flash Web Application """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config():
+    """ Config class. """
+    LANGUAGES = ["en", "fr"]
+
+    Babel.default_locale = "en"
+    Babel.default_timezone = "UTC"
+
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Simply outputs template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
